e150836917b0c566503438cd9655dc07615f77af
VijayJsDev <vijayb2097@gmail.com>
2024-02-28 21:37:09 +0530

    mongodb setup has been added

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Signup() {
+  const [fullname, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("")
 
   async function handleSubmit(e) {
-    e.preventDefault();
-
-    const response = await fetch("http://localhost:5050/signup", {
-      
-    })
+    e.preventDefault()
+    
+    try{
+      const response = await fetch("http://localhost:5050/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({fullname, email, password, confirmPassword}),
+    });
+    } catch(err){
+      console.err("error while submitting", err);
+    }
   }
-
   return (
     <>
       <div>
@@ -18,25 +30,47 @@ function Signup() {
         <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Full Name</label>
-            <input type="text" placeholder="Enter Your Full Name" />
+            <input
+              type="text"
+              placeholder="Enter Your Full Name"
+              value={fullname}
+              onChange={(e) => setFullName(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="email">Email Address</label>
-            <input type="email" placeholder="example123@gmail.com" />
+            <input
+              type="email"
+              placeholder="example123@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input type="password" placeholder="Enter Your Password" />
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="confirm-password">Confirm Password</label>
-            <input type="password" placeholder="Confirm Password" />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
           </div>
           <div>
             <input type="submit" />
           </div>
         </form>
-        <p>Existing User? <Link to="/login">Login</Link></p>
+        <p>
+          Existing User? <Link to="/login">Login</Link>
+        </p>
       </div>
     </>
   );
--- server/server.js
+++ server/server.js
@@ -1,9 +1,46 @@
 import express from 'express';
-
+import { MongoClient, ServerApiVersion } from "mongodb";
+import cors from 'cors';
+import bodyParser from 'body-parser';
 const PORT = process.env.PORT || 5050
 
 const app = express();
+app.use(cors());
+app.use(bodyParser());
 
+//MongoDB
+
+
+const URI = process.env.ATLAS_URI || "";
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+try {
+  await client.connect();
+
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+    "Pinged Your Deployment. You Successfully connected to MongoDB!!"
+  );
+} catch (err) {
+  console.error(err);
+}
+
+let db = client.db("mmrl");
+let signupCollection = db.collection("mmrlSignup");
+
+
+const router = express.Router();
+
+app.post("/signup", async (req, res) => {
+    const newUser = req.body;
+    const insertOneUser = signupCollection.insertOne(newUser);
+})
 app.listen(PORT, () => {
     console.log(`Backend Express Server Is Running On PORT ${PORT}`)
 })
