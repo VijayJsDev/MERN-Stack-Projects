d51f383087bdc549e22e687fadb898a49c596d4b
VijayJsDev <vijayb2097@gmail.com>
2024-02-28 20:16:27 +0530

    redux related code removed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import MainNavigation from "./MainNavigation";
 import { Link } from "react-router-dom";
-
-import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "../store/counterSlice";
-
 function Home() {
-  const count = useSelector((state) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
     <>
       <MainNavigation />
@@ -16,11 +9,6 @@ function Home() {
       <p>Book Your Metro Tickets And Enjoy The Ride!</p>
       <h1>Get Started</h1>
       <Link to="/signup">SignUp Now!!</Link>
-      <div>
-        <h1>Counter: {count}</h1>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-      </div>
     </>
   );
 }
