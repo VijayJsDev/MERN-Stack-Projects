92f2da453dcbc3b716b934ee106d6a81e61d7341
VijayJsDev <vijayb2097@gmail.com>
2024-02-29 21:46:56 +0530

    login related code for both client&server side added!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Booking from "./components/Booking";
 
 function App() {
   const router = createBrowserRouter([
     { path: "/", element: <Home /> },
     { path: "/signup", element: <Signup /> },
     { path: "/login", element: <Login /> },
+    { path: "/ticket-booking", element: <Booking /> },
   ]);
 
   return <RouterProvider router={router}></RouterProvider>;
